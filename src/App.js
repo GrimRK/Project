33d0d65a952bc150c005cc075104aec88cd28df7
Aman Kumar <amanv228@gmail.com>
2021-08-31 15:04:23 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+//imports
+import React,{useState} from 'react'
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //variables
+  const [keys,setKeys]=useState("");
+  const [data,setData]=useState({
+    symbol: "",
+  });
+
+//functions
+
+//submit functions
+  function submit(e){
+    e.preventDefault();
+    const symbol=data.symbol;
+    const url="https://cloud.iexapis.com/stable/stock/"+symbol+"/company/quote?token=pk_7b5d7f3300d343af9d1fd122c3a77e82";
+    axios.get(url)
+    .then(res=>{
+      setKeys(res.data)
+    })
+    
 }
+//handle function
+function handle(e)
+{
+  const newdata ={...data}
+  newdata[e.target.id]=e.target.value
+  setData(newdata)
+  console.log(newdata)
+}
+
+//content stores initial content
+ let content = null;
+ content=(<div>
+  <div>
+    <form onSubmit={(e)=>submit(e)}>
+      <label>
+        Symbol :
+      </label>
+      <input onChange={(e)=>handle(e)} type="text" id="symbol" placeholde="Symbol"name="symbol" value={data.symbol}>
+      </input>
+      <button type="submit">Check</button>
+    </form>
+ </div>
+  </div>);
+  
+  if(keys)
+  {
+    content=(<div>
+      
+      <div>
+        <form onSubmit={(e)=>submit(e)}>
+        <label>
+        Symbol :
+      </label>
+        <input onChange={(e)=>handle(e)} type="text" id="symbol" placeholde="Symbol"name="symbol" value={data.symbol}>
+          </input>
+          <button type="submit">Check</button>
+        </form>
+     </div>
+     <div>
+        <h1>Company Name : {keys.companyName}</h1>
+        <h1>website : {keys.website}</h1>
+        <h1>Description :</h1> <p>{keys.description}</p>
+        <h1>CEO : {keys.CEO}</h1>
+        <h1>sector : {keys.sector}</h1>
+      </div>
+
+
+      </div>
+      )
+      
+  }
+ 
+    return(
+      <><div>
+        {content}
+      </div>
+        
+     </>
+  );
+  
+   
+ }
 
 export default App;
